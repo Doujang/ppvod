d4e7813265c6a40a0cd1fe57f907847addaeb626
ali-jinpeng <jinpeng@ali.com>
2018-01-21 19:47:55 +0800

    thinkphp

--- Lib/Conf/config.php
+++ Lib/Conf/config.php
@@ -4,8 +4,8 @@ $array = array(
 	'USER_AUTH_KEY'=>'feifeicms',// 用户认证SESSION标记
 	'NOT_AUTH_ACTION'=>'index,show,add,top,left,main',// 默认无需认证操作
 	'REQUIRE_AUTH_MODULE'=>'Admin,List,Vod,News,User,Collect,Data,Upload,Link,Ads,Cache,Create,Tpl,Cm,Gb,Tag,Special,Nav,Side,Pic',// 默认需要认证模型
-	'URL_PATHINFO_DEPR'=>'-',
-	'URL_MODEL' => 3,
+	'URL_PATHINFO_DEPR'=>'-',// 修改url的分隔符,c/function->c-function
+	'URL_MODEL' => 3,//设置四种url模式0普通1默认pathinfo2REWRITE模式3兼容模式
 	'APP_GROUP_LIST'=>'Admin,Home,Plus',//项目分组
 	'TMPL_FILE_DEPR'=>'_',//模板文件MODULE_NAME与ACTION_NAME之间的分割符，只对项目分组部署有效
 	'LANG_SWITCH_ON'=>true,// 多语言包功能
@@ -16,16 +16,16 @@ $array = array(
 	'TMPL_ACTION_ERROR'     => './Public/jump/jumpurl.html', // 默认错误跳转对应的模板文件
 	'TMPL_ACTION_SUCCESS'   => './Public/jump/jumpurl.html', // 默认成功跳转对应的模板文件		
 	'DATA_PATH_LEVEL'=>2,
-  'ADMIN_TIME_LIMIT' => '300',
-  'HOME_PAGENUM' => '3',
-  'HOME_PAGEGO' => 'pagego',
+    'ADMIN_TIME_LIMIT' => '300',
+    'HOME_PAGENUM' => '3',
+    'HOME_PAGEGO' => 'pagego',
 	//3.3 add
 	'TMPL_DETECT_THEME'=>true,
 	'VAR_TEMPLATE'=>"theme",
-	'APP_DEBUG'       => false,    // 是否开启调试模式
+	'APP_DEBUG'       => false,    // 是否开启调试模式,开启将不生成缓存文件,修改立即生效.可在入口文件中定义常量开启.
   //'SHOW_RUN_TIME'		=> true,   // 运行时间显示
   //'SHOW_ADV_TIME'		=> true,   // 显示详细的运行时间
-  'SHOW_DB_TIMES'		=> true,   // 显示数据库查询和写入次数
+    'SHOW_DB_TIMES'		=> true,   // 显示数据库查询和写入次数
 );
 //1454e6883bb03a0341c549531ff5087c
 return array_merge($config,$array);
--- index.php
+++ index.php
@@ -21,8 +21,13 @@ $App->run();
 //define('APP_NAME','Home');
 //2确定应用路径.死记硬背的
 //define('APP_PATH','./Home')
-//3.引入核心文件
-//require './Lib/thinkPHP/thinkPHP.php'
+//3是否开启debug模式
+//define('APP_DEBUG',true);
+//4.引入核心文件
+//require './Lib/thinkPHP/thinkPHP.php '
+//访问路径去掉index.php
+//1.在apache服务器配置文件http.conf中修改rewrite.去掉#号
+//2.在项目根目录添加h..重写文件.
 
 
 ?>
